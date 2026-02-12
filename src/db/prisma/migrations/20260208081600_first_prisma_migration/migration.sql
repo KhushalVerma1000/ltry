10c73c68116b28699d5e9eb9c8ab8f0bf92869ee
KhushalVerma100 <khushal.verma1000@gmail.com>
2026-02-12 15:53:33 +0530

    basic backend set up

---
+++ src/db/prisma/migrations/20260208081600_first_prisma_migration/migration.sql
@@ -0,0 +1,76 @@
+-- CreateTable
+CREATE TABLE "AdminUser" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "AdminUser_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "publicId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "phone" BIGINT NOT NULL,
+    "refreshToken" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Pool" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "perSeatPrice" DECIMAL(65,30) NOT NULL,
+    "totalSeats" INTEGER NOT NULL,
+    "publicId" TEXT NOT NULL,
+
+    CONSTRAINT "Pool_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Seats" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "poolId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "status" TEXT NOT NULL,
+
+    CONSTRAINT "Seats_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "payments" (
+    "id" SERIAL NOT NULL,
+    "amount" DECIMAL(65,30) NOT NULL,
+    "provider" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "seatsId" INTEGER NOT NULL,
+
+    CONSTRAINT "payments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "AdminUser_email_key" ON "AdminUser"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_publicId_key" ON "User"("publicId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "User"("phone");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Pool_publicId_key" ON "Pool"("publicId");
+
+-- AddForeignKey
+ALTER TABLE "Seats" ADD CONSTRAINT "Seats_poolId_fkey" FOREIGN KEY ("poolId") REFERENCES "Pool"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Seats" ADD CONSTRAINT "Seats_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "payments" ADD CONSTRAINT "payments_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
