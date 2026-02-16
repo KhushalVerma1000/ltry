f6bed39cc43b3f9fbca70257bbe6e8e2c87754fe
KhushalVerma100 <khushal.verma1000@gmail.com>
2026-02-16 11:55:21 +0530

    configurations and basic set up

---
+++ src/db/index.ts
@@ -0,0 +1,26 @@
+// src/db/index.ts
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client.js";
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+    throw new Error("DATABASE_URL is not defined");
+}
+
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+const connectDB = async () => {
+    try {
+        await prisma.$connect();
+        console.log("Connected to database successfully");
+    } catch (error) {
+        console.error("Database connection failed:", error);
+        throw error;
+        process.exit(1)
+    }
+};
+
+export { prisma, connectDB };
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { error } from "node:console";
+import { app } from "./app.js";
+import {connectDB} from "./db/index.js";
+
+
+
+
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log("Server is running on port ",process.env.PORT)
+    })
+})
+.catch((error)=>{
+    console.error("Failed to connect to database:", error);
+})
